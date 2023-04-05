7218545b8ae5919315e81a311cc457e70805920f
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-04-05 15:44:01 +0800

    Navbar buttons update

--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -3,21 +3,96 @@ import DashboardScreen from "../screens/dashboard-screen";
 import HistoryScreen from "../screens/history-screen";
 import AlertScreen from "../screens/alert-screen";
 import ProfileScreen from "../screens/profile-screen";
+import HomeScreen from "../screens/home-screen";
 
-const MainTab = createBottomTabNavigator();
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <MainTab.Navigator
+    <Tab.Navigator
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          height: 80,
+          elevation: 0,
+          borderRadius: 5,
+        },
       }}
     >
-      <MainTab.Screen name="Dashboard" component={DashboardScreen} />
-      <MainTab.Screen name="History" component={HistoryScreen} />
-      <MainTab.Screen name="Alert" component={AlertScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
-    </MainTab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="home"
+              solid
+              color={focused ? "#000000" : "#808080"}
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Alert"
+        component={AlertScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="car"
+              solid
+              color={focused ? "#000000" : "#808080"}
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="car"
+              solid
+              color={focused ? "#000000" : "#808080"}
+              size={35}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="th-large"
+              solid
+              color={focused ? "#000000" : "#808080"}
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="user"
+              solid
+              color={focused ? "#000000" : "#808080"}
+              size={25}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
