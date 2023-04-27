eba1e2eb61f22f4c9920e8366b0c0635108c3025
Lawrence Pinlac <lcbpinlac2019@plm.edu.ph>
2023-04-27 17:50:23 +0800

    major update: alert page

--- src/components/HistoryCard.js
+++ src/components/HistoryCard.js
@@ -1,8 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { firebase } from "../../firebase";
+import { QuerySnapshot } from "firebase/firestore";
 
-const HistoryCard = () => {
+const HistoryCard = ({  }) => {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const db = firebase.firestore();
+  //   const currentUser = firebase.auth().currentUser;
+
+  //   if (currentUser) {
+  //     const unsubscribe = db
+  //       .collection("detected")
+  //       .where("UserID", "==", currentUser.uid)
+  //       .orderBy("dateSeen", "desc")
+  //       .onSnapshot((querySnapshot) => {
+  //         const items = [];
+  //         querySnapshot.forEach((doc) => {
+  //           const data = doc.data();
+  //           items.push({
+  //             dateSeen: data.dateSeen,
+  //             timeSeen: data.timeSeen,
+  //           });
+  //         });
+  //         // Sort items in descending order based on dateSeen field
+  //         items.sort((a, b) => b.dateSeen.localeCompare(a.dateSeen));
+  //         setData(items);
+  //       });
+  //     return () => unsubscribe();
+  //   }
+  // }, []);
+
   return (
     <View style={styles.container}>
       <FontAwesome5
